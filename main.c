58db76e1a4dc1fe346c5d0126396d45efae5552e
unknown <jose_rock67@hotmail.com>
2015-10-27 11:22:15 -0600

    Problema 17

--- main.c
+++ main.c
@@ -3,8 +3,16 @@
 #include <conio.h>
 void main()
 {
-  int n;
-  printf("Hola, Mundo\n");
+  int n,i,j,k;
+  scanf("%d",&i);
+  for(n=1;n<=i;n++){
+    for(j=1;j<=n;j++){
+       printf("%d",j);
+    }
+    for(k=j-2;k>=1;k--){
+       printf("%d",k);
+    }
+    printf("\n");
+  }
   getch();
-
 }
